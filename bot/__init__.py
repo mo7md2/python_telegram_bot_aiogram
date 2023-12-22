42ffff8222aeff1bfede5cb1f16e78fac546c569
mo7md <ksa.mo7md@gmail.com>
2023-12-22 22:31:11 +0300

    fix

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,11 +1,10 @@
-from bot.handlers import give_contacts, give_info, give_settings, start_message, text_handler, unknown_message
+from bot.handlers import give_contacts, give_info, start_message, text_handler, unknown_message
 
 
 # list of imported functions from module handlers
 __all__ = [
     "give_contacts",
     "give_info",
-    "give_settings",
     "start_message",
     "text_handler",
     "unknown_message",
